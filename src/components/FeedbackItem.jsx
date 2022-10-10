820f516d0e6f143a28c06067dfec9c59d15f2035
Yewdev <Y_cooldev@outlook.com>
2022-10-10 17:27:35 +0700

    Update Feedback handleDelete

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,11 +2,13 @@ import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from "./shared/Card"
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
+
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button className="close">
+      <button onClick={() => handleDelete(item.id)} className="close">
         <FaTimes color='red' />
       </button>
       <div className="text-display">{item.text}</div>
