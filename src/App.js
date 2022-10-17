d939335de00b514776e5b1692cb29e1bd906304b
Yewdev <Y_cooldev@outlook.com>
2022-10-18 02:16:52 +0700

    Create Router and AbouPage (not link yet)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
 import Header from './components/Header'
-import FeedbackList from './components/FeedbackList';
-import FeedbackStats from './components/FeedbackStats';
-import FeedbackForm from './components/FeedbackForm';
-import FeedbackData from './data/FeedbackData';
-
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import FeedbackData from './data/FeedbackData'
+import AboutPage from './components/pages/AboutPage'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -14,23 +15,39 @@ function App() {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
   }
-  
-  const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
 
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
   return (
     <>
-      <Header />
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-      </div>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList
+                    feedback={feedback}
+                    handleDelete={deleteFeedback}
+                  />
+                </>
+              }
+            ></Route>
+
+            <Route path="/about" element={<AboutPage />}></Route>
+          </Routes>
+        </div>
+      </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
