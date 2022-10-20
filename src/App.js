387582f05012caaa8125444bbb79f76a64a23549
Yewdev <Y_cooldev@outlook.com>
2022-10-20 13:14:12 +0700

    Move Add,Del Function to Context

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import { v4 as uuidv4 } from 'uuid'
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
-import FeedbackData from './data/FeedbackData'
 import AboutPage from './components/pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
 import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-  }
-
-  const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
+  
   return (
     <>
       <FeedbackProvider>
@@ -35,9 +22,9 @@ function App() {
                 path="/"
                 element={
                   <>
-                    <FeedbackForm handleAdd={addFeedback} />
+                    <FeedbackForm />
                     <FeedbackStats />
-                    <FeedbackList handleDelete={deleteFeedback} />
+                    <FeedbackList />
                   </>
                 }
               ></Route>
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,15 @@
+import { useContext } from 'react'
 import {FaTimes} from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Card from "./shared/Card"
-
-function FeedbackItem({ item, handleDelete }) {
-
+import FeedbackContext from '../context/FeedbackContext'
+function FeedbackItem({ item}) {
+  const {deleteFeedback } = useContext(FeedbackContext)
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)} className="close">
+      <button onClick={() => deleteFeedback(item.id)} className="close">
         <FaTimes color='red' />
       </button>
       <div className="text-display">{item.text}</div>
