e0c18ef67e0fcf9d7929838f8bafe80557348c88
Yewdev <Y_cooldev@outlook.com>
2022-11-17 23:08:50 +0700

    Update Add Feedback function post in DB

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect } from 'react'
-import { v4 as uuidv4 } from 'uuid'
+
 
 const FeedbackContext = createContext()
 
@@ -18,7 +18,7 @@ export const FeedbackProvider = ({children}) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(`http://localhost:5001/feedback?sort=id&_order=desc`)
+    const response = await fetch(`/feedback?sort=id&_order=desc`)
     const data = await response.json()
 
     setFeedback(data)
@@ -26,9 +26,17 @@ export const FeedbackProvider = ({children}) => {
   }
 
   // Add feedback
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeedback),
+    })
+
+    const data = await response.json()
+    setFeedback([data, ...feedback])
   }
 
   // Delete feedback
